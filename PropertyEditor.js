3ec327a648da5c535007aaa19c9fc82cc96fa6bd
sombikroy <111582949+sombikroy@users.noreply.github.com>
2026-03-20 01:29:08 +0530

    Add files via upload

---
+++ PropertyEditor.js
@@ -0,0 +1,43 @@
+sap.ui.define([
+    "sap/ui/model/resource/ResourceModel",
+    "sap/dm/dme/podfoundation/control/PropertyEditor"
+], function (ResourceModel, PropertyEditor) {
+    "use strict";
+    
+    var oFormContainer;
+
+    return PropertyEditor.extend( "sb.custom.plugins.manageuom.builder.PropertyEditor" ,{
+
+		constructor: function(sId, mSettings){
+			PropertyEditor.apply(this, arguments);
+			
+			this.setI18nKeyPrefix("customComponentListConfig.");
+			this.setResourceBundleName("sb.custom.plugins.manageuom.i18n.builder");
+			this.setPluginResourceBundleName("sb.custom.plugins.manageuom.i18n.i18n");
+		},
+		
+		addPropertyEditorContent: function(oPropertyFormContainer){
+			var oData = this.getPropertyData();
+			
+			this.addSwitch(oPropertyFormContainer, "backButtonVisible", oData);
+			this.addSwitch(oPropertyFormContainer, "closeButtonVisible", oData);
+						
+			this.addInputField(oPropertyFormContainer, "title", oData);
+			this.addInputField(oPropertyFormContainer, "text", oData);
+
+            oFormContainer = oPropertyFormContainer;
+		},
+		
+		getDefaultPropertyData: function(){
+			return {
+				
+				"backButtonVisible": true,
+				"closeButtonVisible": true,
+                "title": "manageuom",
+				"text": "manageuom"
+                
+			};
+		}
+
+	});
+});
